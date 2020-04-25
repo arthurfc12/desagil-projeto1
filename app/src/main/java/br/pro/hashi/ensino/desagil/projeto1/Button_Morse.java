39761c95e50b946bf994f634d88451e21249756a
VergaraC <victorvaac@al.insper.com.br>
2020-04-25 19:58:56 -0300

    Botao de Morse funcionando, digita .&-

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Button_Morse.java
@@ -0,0 +1,33 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Button_Morse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button__morse2);
+
+        TextView escrita = findViewById(R.id.Title_button);
+        Button button_morse = findViewById(R.id.ButtonMorse);
+
+        button_morse.setOnClickListener((view) -> {
+            String text = escrita.getText().toString();
+            String dado = ".";
+            String content = text+ dado;
+            escrita.setText(content);
+        });
+        button_morse.setOnLongClickListener((view) -> {
+            String text = escrita.getText().toString();
+            String dado = "-";
+            String content = text+ dado;
+            escrita.setText(content);
+            return true;
+        });
+    }
+}
